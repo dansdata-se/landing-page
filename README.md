6d095f3ff77d1405276ab78726ebcfe37e5fa299
Felix Zedén Yverås <felix@fzy.se>
2022-06-09 12:46:00 +0200

    Initial version of landing page

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dansdata.se Landing Page
+
+This is a simple landing page for https://dansdata.se while
+development on the overall system is ongoing.
+
+## License
+
+MIT
+
+The [logo](./src/logo.webp), [vargarda.webp](./src/vargarda.webp) and
+[vargarda-1200w.webp](./src/vargarda-1200w.webp) assets are licensed
+under CC-BY-SA-4.0.
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      light: {
+        primary: "#006877",
+        "on-primary": "#ffffff",
+        secondary: "#4b6268",
+        tertiary: "#006b58",
+        outline: "#6f797b",
+        background: "#fbfcfd",
+        "on-background": "#191c1d",
+      },
+      dark: {
+        primary: "#52d7f0",
+        "on-primary": "#00363f",
+        secondary: "#b2cbd1",
+        tertiary: "#58dcbc",
+        outline: "#899295",
+        background: "#191c1d",
+        "on-background": "#e1e3e4",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {
+      backgroundImage: {
+        vargarda: "url('/src/vargarda.webp')",
+        vargarda1200w: "url('/src/vargarda-1200w.webp')",
+      },
+    },
+  },
+  plugins: [],
+};
